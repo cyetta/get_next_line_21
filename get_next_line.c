b03b687bf6530441d23179c7e02259b9b1d3687d
Cari Yetta <cyetta@student.msk.21-school.ru>
2021-10-24 19:41:45 +0300

    upd

--- get_next_line.c
+++ get_next_line.c
@@ -6,26 +6,46 @@
 /*   By: cyetta <cyetta@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 00:14:13 by cyetta            #+#    #+#             */
-/*   Updated: 2021/10/24 06:19:16 by cyetta           ###   ########.fr       */
+/*   Updated: 2021/10/24 19:29:22 by cyetta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdlib.h>
 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE = 4096
+# define BUFFER_SIZE 4096
 #endif
+/*
+Returnt actual filedescriptor and pointer to fl_desc
+if not exist, Create fl_desc and init it.
+in creation error return NULL
+*/
+fl_desc	*createfdesc(fl_desc **fdesc, int fd)
+{
+	if (!(*fdesc))
+	{
+		*fdesc = (fl_desc *)malloc(sizeof(fl_desc));
+		if (!(*fdesc))
+			return (NULL);
+		(*fdesc)->buf = (char *)malloc(sizeof(char) * BUFFER_SIZE);
+		if (!(*fdesc)->buf)
+		{
+			free(*fdesc);
+			return (NULL);
+		}
+		(*fdesc)->fd = fd;
+		(*fdesc)->idx = 0;
+	}
+	return (*fdesc);
+}
 
 char	*get_next_line(int fd)
 {
-	char		*dst;
-	static int	cnt;
+	static fl_desc	*fdesc;
 
-	if (!cnt)
-		cnt = fd;
-	if (--cnt)
-		dst = ft_strdup("test\n");
-	else
-		dst = NULL;
-	return (dst);
+	if (!createfdesc(&fdesc, fd))
+		return (NULL);
+
+	return (NULL);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -13,6 +13,12 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include <stddef.h>
+struct s_fdesc
+{
+	char	*buf;
+	int		idx;
+	int		fd;
+} typedef fl_desc;
 
 char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
